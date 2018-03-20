85aa3c5cfed4435dd6c386658f80063bc3d03e47
Nik Gavalas <gavalnick@gmail.com>
2018-03-20 22:15:04 +0200

    advancements

--- src/easyargs.cpp
+++ src/easyargs.cpp
@@ -1,58 +1,227 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <map>
+#include <sstream>
+#include <algorithm>
+
+#define DEBUG
 
 class Argument {
 public:
-private:
+	std::string Short;
+	std::string Long;
+	std::string Helptext;
+	bool Required;
+
+	Argument(std::string shrt, std::string lng, std::string helptext, bool required) {
+		this->Short = shrt;
+		this->Long = lng;
+		this->Helptext = helptext;
+		this->Required = required;
+	}
 };
 
 class EasyArgs {
 public:
-	EasyArgs(char argc, char *argv[]) {
-		for (int i = 0; i < argc; i++) {
+	EasyArgs(char argc, char *argv[]) 
+	{
+		for (int i = 0; i < argc; i++)
 			this->args.push_back(std::string(argv[i]));
-		}
-		/* for (std::string s : this->args) {
-			std::cout << s << std::endl;
-		} */ 
+		
+		this->InitUsage();
+		this->ExplodeFlags();
 	};
 
-	~EasyArgs() {};
+	~EasyArgs() {}
 
-	void AddFlag(std::string shrt, std::string lng, std::string help) {};
-	void AddValue(std::string shrt, std::string lng, std::string help, std::string req) {};
-	void AddPositional(std::string name) {};
-	void Parse() {};
-	bool IsSet(std::string argname) {return false; };
-	std::string GetValueFor(std::string argname) {return "a";};
-	std::string GetPositional(std::string argname) {return "f";};
+	EasyArgs* Version(std::string version) 
+	{
+		this->version = version;
+		return this;
+	}
+
+	EasyArgs* Description(std::string desc) 
+	{
+		this->description = desc;
+		return this;
+	}
+
+	EasyArgs* Flag(std::string shrt, std::string lng, std::string help) 
+	{
+		this->AddUsageOption(shrt, lng, help, false);
+		this->expectedArgs.push_back(new Argument(shrt, lng, help, false));
+		return this;
+	}
+
+	EasyArgs* Value(std::string shrt, std::string lng, std::string help, bool required) 
+	{
+		this->AddUsageOption(shrt, lng, help, required);
+		this->expectedArgs.push_back(new Argument(shrt, lng, help, required));
+		return this;
+	}
+
+	EasyArgs *Positional(std::string argname, std::string help)
+	{
+		this->usagePos << argname << ' ';
+		this->expectedArgs.push_back(new Argument(argname, argname, help, true));
+		return this;
+	};
+
+	bool IsSet(std::string argname) 
+	{
+		Argument* ret = GetArgByName(argname);
+
+		if (!ret)
+			return false;
+
+		return std::find(this->args.begin(), this->args.end(), '-' + ret->Short) != this->args.end() 
+			|| std::find(this->args.begin(), this->args.end(), "--" + ret->Long) != this->args.end();
+	}
+	
+	std::string GetValueFor(std::string argname) 
+	{
+		auto it = std::find(this->args.begin(), this->args.end(), '-' + argname);
+		if (it == this->args.end() || it == this->args.end() - 1) 
+		{
+			it = std::find(this->args.begin(), this->args.end(), "--" + argname);
+			if (it == this->args.end() || it == this->args.end() - 1) 
+			{
+				std::ostringstream err;
+				err << '"' << argname << '"' << " not specified";
+				this->Error(err.str());
+			}
+		}
+		std::string ret = *(it + 1);
+		if (ret[0] != '-') 
+		{
+			return ret;
+		}
+		return "penis";
+	}
+
+	std::string GetPositional(std::string name) 
+	{
+		return name; // FIXME
+	}
+	
+	void PrintUsage()
+	{
+		std::cout << this->usage.str();
+		std::cout << this->usagePos.str() << std::endl << std::endl;
+		std::cout << "version: " << this->version << std::endl;
+		std::cout << "description: " << this->description << std::endl << std::endl;
+		std::cout << this->usageOpt.str() << std::endl;
+	}
 
 private:
 	std::vector<std::string> args;
+	std::vector<Argument*> expectedArgs;
+	std::map<std::string, std::string> optValues;
+	std::map<std::string, std::string> posValues;
 
-	void Explode() {};
+	std::stringstream usage;
+	std::stringstream usageOpt;
+	std::stringstream usagePos;
+
+	std::string version;
+	std::string description;
+
+	void InitUsage() 
+	{
+		this->version = "0.0.1";
+		this->description = "-";
+		this->usage << "usage: " << this->args[0] << " [OPTIONS] ";
+		this->usageOpt << "OPTION       REQUIRED   DESCRIPTION\n-----------------------------------\n";
+	}
+
+	void ExplodeFlags() 
+	{
+		/* if s is argument of type '-abc', make it '-a -b -c' */
+		int argc = this->args.size();
+
+		for (int i = 0; i < argc; i++) 
+		{
+			std::string s = this->args[i];
+
+			/* push back the exploded args */
+			if (this->IsMultipleFlagsArg(s)) 
+			{
+				for (unsigned int i = 1; i < s.length(); i++) 
+				{
+					std::string ret = "-";
+					ret += s[i];
+					this->args.push_back(ret);
+				}
+			}
+		}
+		/* delete the leftovers */
+		for (auto it = this->args.begin(); it != this->args.end(); ) 
+		{
+			if (this->IsMultipleFlagsArg(*it)) 
+				it = this->args.erase(it);
+			else
+				it++;
+		}
+	};
+
+	bool IsMultipleFlagsArg(std::string s) 
+	{
+		return s.length() > 2 && s[0] == '-' && s[1] != '-';
+	}
+
+	Argument* GetArgByName(std::string name) 
+	{
+		Argument* ret = NULL;
+
+		for (Argument *arg : this->expectedArgs)
+		{
+			if (arg->Short == name || arg->Long == name)
+				ret = arg;
+		}
+
+		return ret;
+	}
+
+	void AddUsageOption(std::string shrt, std::string lng, std::string help, bool required) 
+	{
+		//lng = lng.size() < 8 ? lng + '\t' : lng;
+		std::string req = required ? "yes" : "no";
+		this->usageOpt << shrt + ", " << lng + '\t' << req + '\t' << help << std::endl;
+	}
+
+	void Error(std::string err) 
+	{
+		std::cout << "wrong arguments: " << err << std::endl;
+		this->PrintUsage();
+	}
 };
 
-int main(int argc, char *argv[]) {
-
-	char *raw[] = {"-pa", "-f", "12", "--verbose", "true", "pos1", "-S"};
+// TODO: subcommands
+// TODO: get as specific type
+int main(int argc, char *argv[]) 
+{
+	char *raw[] = {"exec", "-pa", "-f=12", "--foo=true", "pos1", "-Sw"};
 	int raw_len = 7;
 
 	EasyArgs *ez = new EasyArgs(raw_len, raw);
 
-	ez->AddFlag("-b", "--bacon", "bacon help message");
-	ez->AddValue("-f", "--foo", "foo help message", "required");
-	ez->AddValue("-p", "--pizza", "everybody likes pizza", "optional");
-	ez->AddPositional("name");
+	ez->Version("0.1")
+		->Description("Example")
+		->Flag("b", "bacon", "bacon help message")
+		->Flag("v", "verbose", "be verbose")
+		->Value("f", "foo", "foo help message", true)
+		->Value("p", "pizza", "everybody likes pizza", false)
+		->Positional("name", "your name")
+		->Positional("nom",  "om nom")
+		->PrintUsage();
 
-	ez->Parse();
-
-	if (ez->IsSet("b")) {
-		std::cout << "True";
-	}
-	std::cout << "Value for p: " << ez->GetValueFor("p") << std::endl;
+	if (ez->IsSet("v"))
+		std::cout << "True" << std::endl;
+	
+	std::cout << "Value for optional: " << ez->GetValueFor("f") << std::endl;
 	std::cout << "Value for positional: " << ez->GetPositional("name");
+
+	delete ez;
 
 	return 0;
 }
